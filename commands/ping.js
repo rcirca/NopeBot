fc170ecd38e1f48dd574329644c8f9fb1b12305e
Robert <failquality@gmail.com>
2020-03-10 20:10:20 -0700

    ping missing description

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
 module.exports = {
     name: 'ping',
+    description: 'check latency of bot and API',
     execute(message) {
         message.channel.send('...')
             .then(msg => msg.edit(`Latency: ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(message.client.ping)}ms`))
